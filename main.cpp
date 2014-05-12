7539cb0256318bf50c3d8dc5d9b320a2ef4a2602
Park June Chul <pjc0247@naver.com>
2014-05-13 01:12:32 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
-#include <ruby/ruby.h>
+#include <Ruby/Ruby.h>
 #include <stdio.h>
 
 int main(int argc, char **argv){
     ruby_sysinit( &argc, &argv );
     ruby_init();
 
-    test_eval();
-    test_eval2();
-    test_value();
-    test_value2();
-    test_funcall();
-    test_deffunc();
-    test_deffunc2();
-    test_defclass();
-    test_defmethod();
+    /*
+        testcode here
+    */
     
     ruby_cleanup( 0 );
     return 0;
