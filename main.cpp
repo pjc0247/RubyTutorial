dd993844aef953903f3ca47d92ed7ca3c79b502d
Park June Chul <pjc0247@naver.com>
2014-05-12 23:47:14 +0900

    add test_global

--- main.cpp
+++ main.cpp
@@ -39,6 +39,15 @@ void test_value2(){
     printf("test_value2 / %d %d\n",
         nil == NULL, nil == Qnil);
 }
+
+void test_global(){
+    VALUE gWhite =
+        rb_gv_set( "$white", INT2NUM(1234) );
+    
+    printf("test_global\n");
+    rb_eval_string("puts $white");
+}
+
 void test_funcall(){
     ID fPuts = rb_intern("puts"); /* id of 'puts' */
     
